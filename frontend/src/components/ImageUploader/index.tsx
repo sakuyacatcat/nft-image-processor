58cec4c8e16250d27fbcf812909c338945f5f76c
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-16 01:36:55 +0900

    feat: tesseract ocn function

--- frontend/src/components/ImageUploader/index.tsx
+++ frontend/src/components/ImageUploader/index.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import ReactCrop, { Crop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
+import * as Tesseract from 'tesseract.js'
 
 export default function ImageUploader() {
   const [imgSrc, setImgSrc] = useState<string | null>(null)
@@ -19,10 +20,33 @@ export default function ImageUploader() {
     imgRef.current = e.currentTarget
   }
 
-  const onCropComplete = (crop: Crop) => {
+  const onCropComplete = async (crop: Crop) => {
+	if (imgRef.current && crop.width && crop.height) {
+	  const canvas = document.createElement('canvas')
+	  const scaleX = imgRef.current.naturalWidth / imgRef.current.width
+	  const scaleY = imgRef.current.naturalHeight / imgRef.current.height
+	  canvas.width = crop.width
+	  canvas.height = crop.height
+	  const ctx = canvas.getContext('2d')!
+	  ctx.drawImage(
+		imgRef.current,
+		crop.x! * scaleX,
+		crop.y! * scaleY,
+		crop.width! * scaleX,
+		crop.height! * scaleY,
+		0,
+		0,
+		crop.width!,
+		crop.height!
+	  )
+
+	  const imageSrc = canvas.toDataURL()
+
+	  const result = await Tesseract.recognize(imageSrc, 'eng')
+	  console.log('OCR Result:', result.data.text)
+	}
 	console.log('Crop:', crop)
   }
-
   const onCropChange = (newCrop: Crop) => {
 	setCrop(newCrop)
   }
@@ -35,7 +59,7 @@ export default function ImageUploader() {
 		  crop={crop}
 		  onComplete={onCropComplete}
 		  onChange={onCropChange}
-		  aspect={16 / 9}
+		//   aspect={16 / 9}
 		>
 		  <img
 			ref={imgRef}
