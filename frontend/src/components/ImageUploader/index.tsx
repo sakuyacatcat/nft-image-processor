9e4aeefec9df3e855ab416f03f935b57ec6f4d59
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-16 01:19:00 +0900

    feat: add crop function for uploaded image

--- frontend/src/components/ImageUploader/index.tsx
+++ frontend/src/components/ImageUploader/index.tsx
@@ -1,40 +1,50 @@
-import React, { useState } from 'react';
-import ReactCrop from 'react-image-crop';
-import 'react-image-crop/dist/ReactCrop.css';
+import React, { useRef, useState } from 'react'
+import ReactCrop, { Crop } from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
 
-const ImageUploader: React.FC = () => {
-  const [image, setImage] = useState<string | null>(null);
-  const [crop, setCrop] = useState<ReactCrop.Crop>({ aspect: 4 / 3 });
+export default function ImageUploader() {
+  const [imgSrc, setImgSrc] = useState<string | null>(null)
+  const [crop, setCrop] = useState<Crop>()
+  const imgRef = useRef<HTMLImageElement | null>(null)
 
-  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    const reader = new FileReader();
+  const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+	if (e.target.files && e.target.files.length > 0) {
+	  const reader = new FileReader()
+	  reader.addEventListener('load', () => setImgSrc(reader.result as string))
+	  reader.readAsDataURL(e.target.files[0])
+	}
+  }
 
-    reader.onloadend = () => {
-      setImage(reader.result as string);
-    };
+  const onImageLoaded = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    imgRef.current = e.currentTarget
+  }
 
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
+  const onCropComplete = (crop: Crop) => {
+	console.log('Crop:', crop)
+  }
+
+  const onCropChange = (newCrop: Crop) => {
+	setCrop(newCrop)
+  }
 
   return (
-    <div>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
-      {image && (
-        <div>
-          <img src={image} alt="Uploaded preview" style={{ maxWidth: '100%' }} />
-          <ReactCrop
-            crop={crop}
-            ruleOfThirds
-            onComplete={newCrop => setCrop(newCrop)}
-            onChange={newCrop => setCrop(newCrop)}
-          />
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default ImageUploader;
+	<div>
+	  <input type="file" accept="image/*" onChange={onSelectFile} />
+	  {imgSrc && (
+		<ReactCrop
+		  crop={crop}
+		  onComplete={onCropComplete}
+		  onChange={onCropChange}
+		  aspect={16 / 9}
+		>
+		  <img
+			ref={imgRef}
+			alt="Crop me"
+			src={imgSrc}
+			onLoad={onImageLoaded}
+		  />
+		</ReactCrop>
+	  )}
+	</div>
+  )
+}
