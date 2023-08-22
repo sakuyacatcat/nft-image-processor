2e7b88d3a26d8ae72d97ed491010523de010392d
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-22 23:39:55 +0900

    feat: first contract test

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,4 +1,6 @@
-require("@matterlabs/hardhat-zksync-solc");
+import "@matterlabs/hardhat-zksync-solc";
+import "@nomiclabs/hardhat-ethers";
+import '@nomiclabs/hardhat-waffle';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,6 +26,10 @@ module.exports = {
       chainId: 324,
       zksync: true,
     },
+    hardhat: {
+      chainId: 31337,
+      initialBaseFeePerGas: 0,
+    },
   },
   paths: {
     artifacts: "./artifacts-zk",
@@ -39,5 +45,8 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  mocha: {
+    timeout: 60_000,
   },
 };
