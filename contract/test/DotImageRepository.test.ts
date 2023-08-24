2ae515920fb27ba67c9106a59f354bc12552d5e4
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-24 16:36:12 +0900

    refactor: construct DotImage unittest

--- contract/test/DotImageRepository.test.ts
+++ contract/test/DotImageRepository.test.ts
@@ -13,10 +13,10 @@ describe("DotImageRepository", function() {
   describe("連長圧縮された svg 文字列を入力に DotImage オブジェクトを作成できる", function() {
     describe("入力された連長圧縮された svg 文字列が適切な場合 DotImage オブジェクトの作成に成功する", function() {
       it("入力が適切な連長圧縮フォーマットであり、32x32 ドット分の文字列の場合 svg オブジェクトを返す", async () => {
-        // 適切に連長圧縮された32x32のドット絵のbytes列
-        const validCompressedSVG = "0x0000000000000000000000000000000000000000000000000000000000000008ff00ff00ff00ff00";
+        // すべて"00"のカラーインデックスの32x32のドット絵のsvgを連長圧縮したbytes列
+        const validCompressedSVG = "0xff00ff00ff00ff00";
         const dotImage = await contract.constructDotImage(validCompressedSVG)
-        expect(dotImage.imageData).to.equal("0x0000000000000000000000000000000000000000000000000000000000000008ff00ff00ff00ff00");
+        expect(dotImage.imageData).to.equal("0xff00ff00ff00ff00");
       });
     });
 
