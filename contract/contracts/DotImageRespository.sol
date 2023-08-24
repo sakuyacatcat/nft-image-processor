a4d5b87a855f377082cb67dda5e12c9935e5513b
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-24 18:05:43 +0900

    feat: add input length validation on DotImage

--- contract/contracts/DotImageRespository.sol
+++ contract/contracts/DotImageRespository.sol
@@ -4,9 +4,24 @@ pragma solidity ^0.8.0;
 import {IDotImageRepository} from "./interface/IDotImageRepository.sol";
 
 contract DotImageRepository is IDotImageRepository {
-    function constructDotImage(bytes calldata inputData) external view override returns (DotImage memory) {
+    function constructDotImage(bytes calldata inputData) external pure override returns (DotImage memory) {
+        _validateRLESvgLength(inputData);
+
         DotImage memory dotImage;
         dotImage.imageData = inputData;
         return dotImage;
+    }
+
+    function _validateRLESvgLength(bytes memory rleSvg) internal pure {
+        require(_decompressedSvgLength(rleSvg) == 1024, "invalidCompressedSvgLength");
+    }
+
+    function _decompressedSvgLength(bytes memory rleSvg) internal pure returns (uint256) {
+        uint256 decompressedBytesLength = 0;
+
+        for (uint256 i = 0; i < rleSvg.length; i += 2) {
+            decompressedBytesLength += uint8(rleSvg[i]);
+        }
+        return decompressedBytesLength;
     }
 }
