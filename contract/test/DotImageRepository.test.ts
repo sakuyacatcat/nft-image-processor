adf2097ec0f7ac022f884630b79cc7ec4ac2da35
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-08-24 18:04:55 +0900

    feat: update unittest construct DotImage error

--- contract/test/DotImageRepository.test.ts
+++ contract/test/DotImageRepository.test.ts
@@ -14,23 +14,23 @@ describe("DotImageRepository", function() {
     describe("入力された連長圧縮された svg 文字列が適切な場合 DotImage オブジェクトの作成に成功する", function() {
       it("入力が適切な連長圧縮フォーマットであり、32x32 ドット分の文字列の場合 svg オブジェクトを返す", async () => {
         // すべて"00"のカラーインデックスの32x32のドット絵のsvgを連長圧縮したbytes列
-        const validCompressedSVG = "0xff00ff00ff00ff00";
+        const validCompressedSVG = "0xff00ff00ff00ff000400";
         const dotImage = await contract.constructDotImage(validCompressedSVG)
-        expect(dotImage.imageData).to.equal("0xff00ff00ff00ff00");
+        expect(dotImage.imageData).to.equal("0xff00ff00ff00ff000400");
       });
     });
 
     describe("入力された連長圧縮された svg 文字列が不正な場合 DotImage オブジェクトの作成に失敗する", function() {
       it("入力が適切な連長圧縮フォーマットであるが、32x32 ドット分の文字列でなく、1文字足りない場合に invalidCompressedSvgLength エラーを返す", async () => {
         // すべて"00"のカラーインデックスの32x32のドット絵よりも1ドット少ないsvgを連長圧縮したbytes列
-        const tooShortCompressedSVG = "0xff00ff00ff00fe00";
-        await expect(contract.constructDotImage(tooShortCompressedSVG)).to.be.revertedWith("invalidCompressedSvgLength");
+        const tooShortCompressedSVG = "0xff00ff00ff00ff000300";
+        await expect(contract.constructDotImage(tooShortCompressedSVG)).to.be.rejectedWith("invalidCompressedSvgLength");
       });
 
       it("入力が適切な連長圧縮フォーマットであるが、32x32 ドット分の文字列でなく、1文字多い場合に invalidCompressedSvgLength エラーを返す", async () => {
         // すべて"00"のカラーインデックスの32x32のドット絵よりも1ドット多いsvgを連長圧縮したbytes列
-        const tooLongCompressedSVG = "0xff00ff00ff00ff000100";
-        await expect(contract.constructDotImage(tooLongCompressedSVG)).to.be.revertedWith("invalidCompressedSvgLength");
+        const tooLongCompressedSVG = "0xff00ff00ff00ff000500";
+        await expect(contract.constructDotImage(tooLongCompressedSVG)).to.be.rejectedWith("invalidCompressedSvgLength");
       });
     })
   })
