b281c0c9c0a581ab56c35fc42ae4d021b7edc057
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-09-10 01:08:03 +0900

    fix: replace repository to deployer storage test

--- contract/test/DotImageStorage.test.ts
+++ contract/test/DotImageStorage.test.ts
@@ -5,29 +5,22 @@ import { ethers } from "hardhat";
 
 describe("DotImageStorage", function() {
   let storage: Contract;
-  let repository: Contract;
 
   before(async () => {
     const deployer = await ethers.getSigner(0);
     const DotImageStorage = await ethers.getContractFactory("DotImageStorage");
     storage = await DotImageStorage.connect(deployer).deploy();
     await storage.deployed();
-
-    const DotImageRepository = await ethers.getContractFactory("DotImageRepository");
-    repository = await DotImageRepository.connect(deployer).deploy();
-    await repository.deployed();
-
-    await storage.connect(deployer).setRepository(repository.address);
-    await repository.connect(deployer).setStorage(storage.address);
+    await storage.connect(deployer).setRepository(deployer.address);
   });
 
   describe("入力された tokenId と DotImage オブジェクトを関連付けて保存できる", function() {
     describe("入力された tokenId が未使用の整数であり、DotImage オブジェクトが適切な場合保存に成功する", function() {
-      it("入力された tokenId が未使用の整数であり、DotImage オブジェクトが適切な場合保存に成功する", async () => {
+      it("tokenId が最小の値の 1 であり、DotImage オブジェクトが適切な場合保存に成功する", async () => {
         const registerId = 1;
         const dotImage = { imageData: '0x' + crypto.randomBytes(10).toString("hex") };
-        await repository.mintDotImage(registerId, dotImage);
-        const storedDotImage = await repository.getDotImage(registerId);
+        await storage.createDotImage(registerId, dotImage);
+        const storedDotImage = await storage.readDotImage(registerId);
         expect(storedDotImage.imageData).to.deep.equal(dotImage.imageData);
       });
     });
