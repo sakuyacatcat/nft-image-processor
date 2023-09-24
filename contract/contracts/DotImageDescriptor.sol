bc5aa9973c4a647914f7b6acd4841a0ac352184b
shingotakagij7a <shingo.takagi.j7a@jp.denso.com>
2023-09-24 23:15:32 +0900

    feat: add color to palette func

--- contract/contracts/DotImageDescriptor.sol
+++ contract/contracts/DotImageDescriptor.sol
@@ -17,7 +17,38 @@ contract DotImageDescriptor is IDotImageDescriptor, Ownable {
     }
 
     function _addColorToPalette(uint8 _paletteIndex, string calldata _color) internal {
+        _colorValidation(_color);
         palettes[_paletteIndex].push(_color);
+    }
+
+    function _colorValidation(string calldata _color) internal pure {
+        require(_isNotNullString(_color), "nullStringColor");
+        require(_isValidColorDigit(_color), "invalidDigitColor");
+        require(_isValidHexColor(_color), "invalidHexStringColor");
+    }
+
+    function _isNotNullString(string calldata _string) private pure returns (bool) {
+        return bytes(_string).length != 0;
+    }
+
+    function _isValidHexColor(string calldata _color) private pure returns (bool) {
+        bytes memory colorBytes = bytes(_color);
+        for (uint256 i = 0; i < colorBytes.length; i++) {
+            bytes1 char = colorBytes[i];
+            if (!_isValidHexChar(char)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function _isValidHexChar(bytes1 _char) private pure returns (bool) {
+        return (_char >= bytes1("0") && _char <= bytes1("9")) || (_char >= bytes1("a") && _char <= bytes1("f"))
+            || (_char >= bytes1("A") && _char <= bytes1("F"));
+    }
+
+    function _isValidColorDigit(string calldata _color) private pure returns (bool) {
+        return bytes(_color).length == 6;
     }
 
     function getPaletteColor(uint8 _paletteIndex, uint256 _colorIndex) external view returns (string memory) {
--- contract/test/DotImageDescriptor.test.ts
+++ contract/test/DotImageDescriptor.test.ts
@@ -29,8 +29,40 @@ describe("DotImageDescriptor", function() {
         });
       });
     });
-    // describe("入力された paletteIndex が 0~255 の整数であるが、color が 6 桁の 16 進数文字列でない場合、カラーパレットへの保存に失敗する", function() {});
-    // describe("入力された paletteIndex が 0~255 の整数以外の場合、カラーパレットへの保存に失敗する", function() {});
+    describe("入力された paletteIndex が 0~255 の整数であるが、color が 6 桁の 16 進数文字列でない場合、カラーパレットへの保存に失敗する", function() {
+      const testCases = [
+        { colorCondition: "5桁の16進数文字列", inputColor: "00000", expectedError: "invalidDigitColor" },
+        { colorCondition: "7桁の16進数文字列", inputColor: "0000000", expectedError: "invalidDigitColor" },
+        { colorCondition: "16進数以外を含む文字列", inputColor: "zzzzzz", expectedError: "invalidHexStringColor" },
+        { colorCondition: "整数", inputColor: 1, expectedError: "nullStringColor" },
+        { colorCondition: "小数", inputColor: 0.1, expectedError: "nullStringColor" },
+        { colorCondition: "空文字列", inputColor: "", expectedError: "nullStringColor" },
+      ]
+
+      testCases.forEach(({colorCondition, inputColor, expectedError}) => {
+        it(`paletteIndex が 0~255 の整数を満たす 0 であるが、color が${colorCondition}である${inputColor}の場合、${expectedError}を返す`, async () => {
+          const paletteIndex = 0;
+          const color = inputColor;
+          await expect(descriptor.addColorToPalette(paletteIndex, color)).to.be.rejectedWith(expectedError);
+        });
+      });
+    });
+
+    describe("入力された color は 6 桁の 16 進数文字列であるが、paletteIndex が 0~255 の整数以外の場合、カラーパレットへの保存に失敗する", function() {
+      const testCases = [
+        { paletteCondition: "下端の0よりも1小さい値", inputPaletteIndex: -1, expectedError: "value out-of-bounds" },
+        { paletteCondition: "上端の255よりも1大きい値", inputPaletteIndex: 256, expectedError: "value out-of-bounds" },
+        { paletteCondition: "小数", inputPaletteIndex: 0.1, expectedError: "underflow" },
+      ];
+
+      testCases.forEach(({paletteCondition, inputPaletteIndex, expectedError}) => {
+        it(`color が 6 桁の 16 進数文字列を満たす '000000' であるが、paletteIndex が${paletteCondition}である${inputPaletteIndex}の場合、${expectedError}を返す`, async () => {
+          const color = "000000";
+          const paletteIndex = inputPaletteIndex;
+          await expect(descriptor.addColorToPalette(paletteIndex, color)).to.be.rejectedWith(expectedError);
+        });
+      });
+    });
   });
 
 });
